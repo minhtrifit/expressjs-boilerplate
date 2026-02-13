e5bf5ec880f8ca180d116db35ae32d94671c8a46
minhtrifit <minhtri.fit@gmail.com>
2026-02-13 10:47:48 +0700

    feat: update auth module

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,76 +1,27 @@
 import { Request, Response, NextFunction } from 'express';
-import Joi from 'joi';
-import { prisma } from '@/libs/prisma';
-import { comparePassword, generateToken, hashPassword } from '@/libs/auth';
-import { Role, UserPayload } from '@/models/User';
 import { HTTP_STATUS } from '@/constants/http-status-code';
-
-export const RegisterSchema = Joi.object({
-  email: Joi.string().required(),
-  fullName: Joi.string().required(),
-  password: Joi.string().required()
-});
-
-export const LoginSchema = Joi.object({
-  email: Joi.string().required(),
-  password: Joi.string().required()
-});
+import { AuthError, authService } from '@/services/auth.service';
 
 export const register = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const { t } = req;
 
-    const { error, value } = RegisterSchema.validate(req.body, {
-      abortEarly: false, // trả về tất cả lỗi
-      allowUnknown: false // không cho field dư
-    });
-
-    if (error) {
-      return res.status(HTTP_STATUS.BAD_REQUEST).json({
-        success: false,
-        data: null,
-        message: error.details.map((err) => ({
-          field: err.path.join('.'),
-          message: err.message
-        }))
-      });
-    }
-
-    const { email, password, fullName } = value;
-
-    // Find user with email
-    const existedUser = await prisma.user.findUnique({
-      where: { email: email }
-    });
-
-    if (existedUser) {
-      return res.status(HTTP_STATUS.BAD_REQUEST).json({
-        success: false,
-        data: null,
-        message: t('auth.user_email_existed')
-      });
-    }
-
-    // Hash user password
-    const hashedPassword = await hashPassword(password);
-
-    const newUser: UserPayload = {
-      email: email,
-      password: hashedPassword,
-      fullName: fullName,
-      role: Role.USER
-    };
-
-    const user = await prisma.user.create({
-      data: newUser
-    });
+    const user = await authService.register(req.validatedBody);
 
     return res.status(HTTP_STATUS.CREATED).json({
       success: true,
       data: user,
       message: t('auth.register_success')
     });
-  } catch (error) {
+  } catch (error: any) {
+    if (error.message === AuthError.EMAIL_EXISTED) {
+      return res.status(HTTP_STATUS.BAD_REQUEST).json({
+        success: false,
+        data: null,
+        message: req.t('auth.user_email_existed')
+      });
+    }
+
     next(error);
   }
 };
@@ -79,73 +30,46 @@ export const login = async (req: Request, res: Response, next: NextFunction) => 
   try {
     const { t } = req;
 
-    const { error, value } = LoginSchema.validate(req.body, {
-      abortEarly: false, // trả về tất cả lỗi
-      allowUnknown: false // không cho field dư
-    });
-
-    if (error) {
-      return res.status(HTTP_STATUS.BAD_REQUEST).json({
-        success: false,
-        data: null,
-        message: error.details.map((err) => ({
-          field: err.path.join('.'),
-          message: err.message
-        }))
-      });
-    }
-
-    const { email, password } = value;
-
-    if (!email || !password) {
-      return res.status(HTTP_STATUS.BAD_REQUEST).json({
-        success: false,
-        data: null,
-        message: t('auth.email_password_required')
-      });
-    }
-
-    // Find user
-    const user = await prisma.user.findUnique({
-      where: { email: email }
-    });
-
-    if (!user) {
-      return res.status(HTTP_STATUS.NOT_FOUND).json({
-        success: false,
-        data: null,
-        message: t('auth.user_not_found')
-      });
-    }
-
-    // Compare password
-    const validPassword = await comparePassword(password, user.password);
-
-    if (!validPassword) {
-      return res.status(HTTP_STATUS.BAD_REQUEST).json({
-        success: false,
-        data: null,
-        message: t('auth.email_password_not_match')
-      });
-    }
-
-    // Generate token
-    const token = generateToken(user);
-
-    if (!token) {
-      return res.status(HTTP_STATUS.UNAUTHORIZED).json({
-        success: false,
-        data: null,
-        message: t('auth.authorized_failed')
-      });
-    }
+    const result = await authService.login(req.validatedBody);
 
     return res.status(HTTP_STATUS.OK).json({
       success: true,
-      data: { ...user, token },
+      data: result,
       message: t('auth.login_success')
     });
-  } catch (error) {
+  } catch (error: any) {
+    if (error.message === AuthError.NOT_FOUND) {
+      return res.status(HTTP_STATUS.NOT_FOUND).json({
+        success: false,
+        data: null,
+        message: req.t('auth.user_not_found')
+      });
+    }
+
+    if (error.message === AuthError.NO_PERMISSION) {
+      return res.status(HTTP_STATUS.BAD_REQUEST).json({
+        success: false,
+        data: null,
+        message: req.t('auth.no_permission')
+      });
+    }
+
+    if (error.message === AuthError.EMAIL_PASSWORD_NOT_MATCH) {
+      return res.status(HTTP_STATUS.BAD_REQUEST).json({
+        success: false,
+        data: null,
+        message: req.t('auth.email_password_not_match')
+      });
+    }
+
+    if (error.message === AuthError.AUTHORZIED_FAILED) {
+      return res.status(HTTP_STATUS.UNAUTHORIZED).json({
+        success: false,
+        data: null,
+        message: req.t('auth.authorized_failed')
+      });
+    }
+
     next(error);
   }
 };
